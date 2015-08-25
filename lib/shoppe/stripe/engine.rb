ad7190425dc96695f7f390b6596aad0da7203f67
Raoul DIFFOUO <diraulo@gmail.com>
2015-08-25 05:21:50 +0200

    ensure translation files are loaded

--- lib/shoppe/stripe/engine.rb
+++ lib/shoppe/stripe/engine.rb
@@ -1,7 +1,10 @@
 module Shoppe
   module Stripe
     class Engine < Rails::Engine
-      
+      config.before_initialize do
+        config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+      end
+
       initializer "shoppe.stripe.initializer" do
         Shoppe::Stripe.setup
         ActiveSupport.on_load(:action_view) do
@@ -9,12 +12,11 @@ module Shoppe
           ActionView::Base.send :include, Shoppe::Stripe::ViewHelpers
         end
       end
-      
+
       config.to_prepare do
         Shoppe::Order.send :include, Shoppe::Stripe::OrderExtensions
         Shoppe::Payment.send :include, Shoppe::Stripe::PaymentExtensions
       end
-      
     end
   end
 end
